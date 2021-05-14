e408628880b2edf03dbd75c92a1110b5694fe3b3
Vsevolod Epinetov <vsevolod@epinetov.com>
2021-05-14 21:54:45 +0300

    added aip back and it should now work with vercel

---
+++ pages/aip.js
@@ -0,0 +1,131 @@
+/*eslint-disable*/
+import React from "react";
+import Head from 'next/head'
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import Header from "components/Header/Header.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Button from "components/CustomButtons/Button.js";
+import Table from "components/Table/Table.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Footer from "components/Footer/Footer.js";
+
+import GetApp from "@material-ui/icons/GetApp";
+
+import aipPageStyle from "assets/jss/nextjs-material-kit-pro/pages/aipPageStyle.js";
+
+import { getAllAIPData } from 'lib/aip'
+
+const useStyles = makeStyles(aipPageStyle);
+
+const createAIPDate = (allAIPData) => {
+  var fullTable = [];
+
+  allAIPData.forEach(airportData => {
+    var row = [airportData.aeroportICAOCode, airportData.aeroportInfo.name, airportData.aeroportInfo.country];
+
+    /*aeroportData.availableAIPList.forEach((aip, id) => {
+      if (aip.type === 'full') fullID = id;
+      if (aip.type === 'STAR') starID = id;
+      if (aip.type === 'SID') sidID = id;
+    });*/
+
+    let possibleAIPTypes = ["full", "STAR", "SID"];
+
+    //console.log(airportData);
+
+    possibleAIPTypes.forEach(type => {
+      if (airportData.availableAIPTypes[type]) {
+        //let link = `https://storage.googleapis.com/atc.epinetov.com/public/aip/${aeroportICAOCode}/${aeroportICAOCode}_${type}.pdf`;
+        row.push(<Button href={`https://storage.googleapis.com/atc.epinetov.com/public/aip/${airportData.aeroportICAOCode}/${airportData.aeroportICAOCode}_${type}.pdf`} color="blue"> <GetApp /> {`${type}`} </Button>)
+      } else {
+        row.push(<Button disabled color="blue"> <GetApp /> {`${type}`} </Button>)
+      }
+
+    })
+
+    /*if (aeroportData.availableAIPTypes.full) row.push(<Button href={aeroportData.availableAIPList[fullID].link} color="blue"> <GetApp /> Полный </Button>)
+    else row.push(<Button disabled color="blue"> <GetApp /> Полный </Button>)
+
+    if (fullID > -1) row.push(<Button href={aeroportData.availableAIPList[fullID].link} color="blue"> <GetApp /> Полный </Button>)
+    else row.push(<Button disabled color="blue"> <GetApp /> Полный </Button>)
+
+    if (starID > -1) row.push(<Button href={aeroportData.availableAIPList[starID].link} color="blue"> <GetApp /> STAR </Button>)
+    else row.push(<Button disabled color="blue"> <GetApp /> STAR </Button>)
+
+    if (sidID > -1) row.push(<Button href={aeroportData.availableAIPList[sidID].link} color="blue"> <GetApp /> SID </Button>)
+    else row.push(<Button disabled color="blue"> <GetApp /> SID </Button>)*/
+
+    fullTable.push(row);
+  })
+
+  console.log(fullTable)
+  return fullTable;
+}
+
+export default function AIPPage({ allAIPData }) {
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+  });
+  const classes = useStyles();
+  return (
+    <div>
+      <Head>
+        <title>AIP | ATC</title>
+      </Head>
+      <Header
+        links={<HeaderLinks dropdownHoverColor="dark" />}
+        color="transparent"
+      />
+      <div className={classes.container}>
+        <GridContainer>
+          <GridItem
+            md={12}
+            className={classes.mlAuto + " " + classes.mrAuto}
+            style={{ marginBottom: '3em !important' }}
+          >
+            <h2 className={classes.title}>Актуальная информация AIP</h2>
+            <h5 className={classes.description}>
+              На этой странице вы можете найти актуальную опубликованную аэронавигационную информацию по аэродромам гражданской авиации и совместного базирования.
+            </h5>
+            <h5 className={classes.description}>
+              Все файлы взяты с <a href="http://www.caiga.ru/" target='_blank'>сайта ЦАИ ГА</a>
+            </h5>
+          </GridItem>
+          <GridItem
+            md={12}
+            className={classes.mlAuto + " " + classes.mrAuto}
+            style={{ marginBottom: '2em !important' }}
+          >
+            <Table
+              striped
+              hover
+              tableHead={[
+                "ICAO",
+                "Название",
+                "Страна",
+                "Полный",
+                "STAR",
+                "SID"
+              ]}
+              tableData={createAIPDate(allAIPData)}
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+      <Footer/>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const allAIPData = getAllAIPData()
+  return {
+    props: {
+      allAIPData
+    }
+  }
+}
